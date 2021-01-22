26a5add20e9ba68a27c0e796f6c714b7f7c174b0
Add & Commit Action <actions@github.com>
2021-01-22 21:22:22 +0800

    feat: support instagram sync

---
+++ _draft/gatsby-timeline-theme.md
@@ -0,0 +1,8 @@
+---
+title: A new Gatsby theme - Timeline,
+date: 2020-11-21
+tags:
+  - 英文
+  - 学习
+  - 单词
+---
---
+++ scripts/sync-instagram.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const fs = require("fs").promises;
+
+const main = async ({ lang = "en" }) => {
+  const outputs = require(`${process.env.GITHUB_WORKSPACE}/${process.env.OUTPUTS_PATH}`);
+  const promises = outputs.map((item) => {
+    const createdAt = new Date(item.timestamp);
+    const tweetFilePath = `./${lang}/instagram/${createdAt.getUTCFullYear()}/${
+      createdAt.getUTCMonth() + 1
+    }/${item.id}.json`;
+    return fs
+      .mkdir(path.dirname(tweetFilePath), {
+        recursive: true,
+      })
+      .then(() => {
+        return fs
+          .writeFile(tweetFilePath, JSON.stringify(item, null, 2), {
+            flag: "wx",
+          })
+          .catch((e) => {
+            if (e.code === "EEXIST") {
+              return Promise.resolve();
+            } else {
+              return Promise.reject(e);
+            }
+          });
+      });
+  });
+  await Promise.all(promises);
+  return outputs.length;
+};
+module.exports = main;
